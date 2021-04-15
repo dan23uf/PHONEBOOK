e000294b23ec79e0d972605c27703517cc7ad09f
claudiabrito96 <claudia.brito@ufl.edu>
2021-04-15 17:35:18 -0400

    Trie implementation

--- phonebook.cpp
+++ phonebook.cpp
@@ -1,5 +1,32 @@
 //
 // Created by danie on 4/11/2021.
 //
-
 #include "phonebook.h"
+#include "contact.h"
+
+void phonebook::insertContact(string name){
+        TrieNode* current = root;
+
+        TrieNode* newNode;
+        for(int i = 0; i < name.length(); i++)
+        {
+            //If node does not exist create a new one
+            if(current->node[name[i]] == NULL){
+                newNode = new TrieNode();
+                current->node[name[i]] = newNode;
+            }
+
+            current = newNode;
+
+            //Marks when the contact ends
+            if(i == name.length()-1)
+               current->last = true;
+        }
+    }
+
+void phonebook::insertIntoTries(string contacts[], int n){
+        root = new TrieNode();
+
+        for(int i = 0; i < n; i++)
+            insertContact(contacts[i]);
+}
--- phonebook.h
+++ phonebook.h
@@ -4,19 +4,41 @@
 
 #ifndef PHONEBOOK_PHONEBOOK_H
 #define PHONEBOOK_PHONEBOOK_H
-
+#include <bits/stdc++.h>
+using namespace std;
 
 class phonebook {
+    private:
+    struct TrieNode{
+        //This map conatins a char (alphabet) points to a TrieNode
+        unordered_map<char, TrieNode*> node;
 
+        bool last;
+        //Constructor
+        TrieNode()
+        {
+            //initialize nodes
+            for(char c = 'a'; c <= 'z'; c++)
+                node[c] = NULL;
+          
+          last = false;
+        }
+    };
 
+    //Root Tree
+    TrieNode* root = NULL;
+
+    public:
     //TODO Show Contact INFO
     //TODO ADD CONTACT MANUALLY
+    void insertContact(string name);
+    //Insert all the Contacts into the Trie
+    void insertIntoTries(string contacts[], int n);
     //TODO EDIT EXISTING CONTACT
     //TODO DELETE INDIVIDUAL CONTACT
     //TODO DELETE ALL CONTACT
 
     //TODO VALIDATIONS
-
     //TODO LOAD CONTACTS FROM FILE or API
     //TODO SAVE CONTACT TO FILE
 
