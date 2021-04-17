8c520ed237ac143c5124ecdf821ee0442f29806a
claudiabrito96 <claudia.brito@ufl.edu>
2021-04-17 16:41:12 -0400

    Text

--- cmake-build-debug/CMakeFiles/PHONEBOOK.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/PHONEBOOK.dir/cmake_clean.cmake
@@ -1,9 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/PHONEBOOK.dir/contact.cpp.o"
-  "CMakeFiles/PHONEBOOK.dir/main.cpp.o"
-  "CMakeFiles/PHONEBOOK.dir/phonebook.cpp.o"
-  "PHONEBOOK"
+  "CMakeFiles/PHONEBOOK.dir/contact.cpp.obj"
+  "CMakeFiles/PHONEBOOK.dir/main.cpp.obj"
+  "CMakeFiles/PHONEBOOK.dir/phonebook.cpp.obj"
+  "PHONEBOOK.exe"
+  "PHONEBOOK.exe.manifest"
   "PHONEBOOK.pdb"
+  "libPHONEBOOK.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include "phonebook.h"
+#include <SFML/Graphics.hpp>
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "PhoneBook");
+    sf::Text implementationOptions;
+    implementationOptions.setString("Choose implementation to use: ");
+    implementationOptions.setCharacterSize(24);
+    implementationOptions.setFillColor(sf::Color::Blue);
+    implementationOptions.setStyle(sf::Text::Bold);
+    implementationOptions.setPosition(10.f,50.f);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(implementationOptions);
+        window.display();
+    }
+
     cout << "Starting..." << endl;
     phonebook _phonebook;
     _phonebook.start();
