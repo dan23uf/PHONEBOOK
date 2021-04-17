b3a76677b40d4fbeb27ed0df87822d2440d47a6a
Vivian Sung <vivian.sung@ufl.edu>
2021-04-17 13:11:00 -0400

    edited cmakelists.txt but this time on clion

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: E:/School/UF/COP3530_PROJECT_3/PHONEBOOK
+# Install script for directory: /Users/vivi/CLionProjects/PHONEBOOK
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/PHONEBOOK")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/School/UF/COP3530_PROJECT_3/PHONEBOOK/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/vivi/CLionProjects/PHONEBOOK/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
